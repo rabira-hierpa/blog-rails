c8edd6bad66360f17ac71505fb67a947ccaef1e8
rabra <rabira.hierpa@gmail.com>
2023-05-04 17:48:21 +0300

    💥 feat: switched to resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'articles#index'
-  # GET Articles
-  get '/articles', to: 'articles#index'
-  # GET Article by id
-  get '/articles/:id', to: 'articles#show'
+
+  resources :articles
 end
