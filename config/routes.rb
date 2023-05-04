e516cd7f7d57343e7fcaf4622f53480d8b86ddc2
rabra <rabira.hierpa@gmail.com>
2023-05-04 17:13:27 +0300

    🚩 feat: added page to display a single article

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "articles#index"
-  
-  get "/articles", to: "articles#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'articles#index'
+  # GET Articles
+  get '/articles', to: 'articles#index'
+  # GET Article by id
+  get '/articles/:id', to: 'articles#show'
 end
